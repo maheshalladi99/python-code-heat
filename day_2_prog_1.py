e4aa64e81053a96c2f6b5bc7d52e7a20115509dc
Mahesh Alladi <alladimaheshbabu333@gmail.com>
2019-05-20 22:22:18 +0530

    Add files via upload

---
+++ day_2_prog_1.py
@@ -0,0 +1,41 @@
+a=10
+b=20
+print(a+b)
+print(a,b,end="\n")
+print(id(a))
+print(id(b))
+print(" ")
+print("about relations and conditionals operators")
+oprend_one = 55
+oprend_two = 65
+print("oprend_one = 55    oprend_two = 65")
+print("oprend_one == oprend_two")
+print(oprend_one == oprend_two)
+print("oprend_one <= oprend_two")
+print(oprend_one <= oprend_two)
+print("oprend_one >= oprend_two")
+print(oprend_one >= oprend_two)
+print("oprend_one != oprend_two")
+print(oprend_one != oprend_two)
+print("about strings")
+string_a="hello people"  #about the strings declaration
+print(string_a)
+print(string_a[0:3])
+print(len(string_a))
+print()
+c = string_a[-3]
+
+print(string_a[0:30:4])
+
+print(string_a[1:5:3])
+print(string_a[-11:-7:3])
+print(string_a[4::-1])
+print(string_a[-8::-1])
+print(string_a[11:5:-1])
+print(string_a[-1:-7:-1])
+s="hai people\
+mahesh"
+print(s)
+
+
+
