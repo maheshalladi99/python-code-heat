f517a344863ac8e4262a43baa9262eb1d4fd3209
Mahesh Alladi <alladimaheshbabu333@gmail.com>
2020-01-28 19:09:38 +0530

    Add files via upload

---
+++ tcs_similar_char.py
@@ -0,0 +1,6 @@
+variable_1=int(input())
+variable_2=input()
+variable_3=int(input())
+for i in range(0,variable_3):
+  variable_4=int(input())
+  print(variable_2.count(variable_2[variable_4 -1],0,variable_4 -1))
